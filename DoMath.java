3006781c337200ae8a0683ad565c824d8b39150b
Kaitlyn Chan <ununoctium829@gmail.com>
2016-09-01 20:49:50 -0700

    round2!

--- DoMath.java
+++ DoMath.java
@@ -2,7 +2,7 @@
 public class DoMath {
 	
 	public static void main (String[] args){
-		double result= Calculate.round2(5.136);
+		double result= Calculate.round2(4.94499);
 		System.out.println(result);
 	}
 
