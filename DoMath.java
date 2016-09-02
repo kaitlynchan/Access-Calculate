a508ecb96f89381155ed2a3b31b8828d8425860a
Kaitlyn Chan <ununoctium829@gmail.com>
2016-09-01 19:42:00 -0700

    hw

--- DoMath.java
+++ DoMath.java
@@ -2,7 +2,7 @@
 public class DoMath {
 	
 	public static void main (String[] args){
-		String result= Calculate.toImproperFrac(3,1,2);
+		double result= Calculate.round2(5.136);
 		System.out.println(result);
 	}
 
