5368213df9fafab9455f0cbe4a24c072d02cc3e0
kaitlynchan <ununoctium829@gmail.com>
2016-10-04 08:57:22 -0700

    final!

--- DoMath.java
+++ DoMath.java
@@ -1,8 +1,12 @@
-
+/*Kaitlyn Chan
+ * Math Library Program (calls the method)
+ * @9/318/16
+ * 1st Period
+ */
 public class DoMath {
 	
 	public static void main (String[] args){
-		int result = Calculate.factorial(3);
+		double result = Calculate.sqrt(0);
 		System.out.println(result);
 	}
 }	
